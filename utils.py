8824613c5571a461c7b947391bafeae63eaeb520
Siddharth Sahay <sahaysid@gmail.com>
2024-09-18 13:59:43 +0200

    write

--- utils.py
+++ utils.py
@@ -1,4 +1,8 @@
-import os
+from tqdm import tqdm
+import glob
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import WebBaseLoader, PyPDFLoader, DirectoryLoader
+
 
 # Output file where you'll store the names
 output_file = 'saved_file_names.txt'
@@ -25,7 +29,53 @@ def read_lancedb():
     pass #todo
 
 def write_new_lancedb():
-    pass
+    for file_path in tqdm(files_to_read, desc="Reading books"):
+        # Load each document using PyPDFLoader
+        loader = PyPDFLoader(file_path)
+        docs = loader.load()
+        all_docs.extend(docs)
+
+    docs = all_docs
+
+    def clean_text(text):
+        # Replace multiple newlines or special characters with a space
+        cleaned = re.sub(r'\n+', ' ', text)
+        # Remove any other special characters (optional)
+        cleaned = re.sub(r'[_\n]', ' ', cleaned)
+        # Normalize spaces (replace multiple spaces with a single space)
+        cleaned = re.sub(r'\s+', ' ', cleaned).strip()
+        return cleaned
+
+    for doc in docs:
+        doc.page_content = clean_text(doc.page_content)
+
+    all_chunks = []
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=50)
+
+    for doc in tqdm(docs, desc="Splitting documents"):
+        chunks = text_splitter.split_documents([doc])
+        all_chunks.extend(chunks)
+
+    for i, chunk in enumerate(all_chunks[100:102]):
+        print(f"Chunk {i + 1}:\n{chunk.page_content}\n")
+
+    query = "Tell me the length of the embeddings for this document"
+    print(len(embeddings.embed_documents([query])[0]))
+
+    db = lancedb.connect("lance_database")
+    table = db.create_table(
+        "rag_tmt",
+        data=[
+            {
+                "vector": embeddings.embed_query("Hello Computer"),
+                "text": "Hello computer!",
+                "id": "1",
+            }
+        ],
+        mode="overwrite",
+    )
+
+    docsearch = LanceDB.from_documents(all_chunks, embeddings, connection=db)
 
 
 '''
